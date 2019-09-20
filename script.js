0b3bf31b034a5d51319aedcc3a3b27f76abdff6f
Han Susan <hansusan@Hans-MacBook-Air.local>
2019-09-20 15:34:30 +0800

    attempted homework assignment not much success

---
+++ script.js
@@ -0,0 +1,30 @@
+// console.log("script js");
+// var inputHappened = function(currentInput){
+// console.log(currentInput);
+//  display
+// emptySeat will be 50
+// planeSeat has 10
+// for every empty seat price goes up 1.05 - 5%
+// };
+// Part1
+
+var PlaneSeats = 10;
+var EmptySeat = 50;
+console.log("SeatPrice" * EmptySeat);
+var seatPrice = function(EmptySeat, NumberOfSeats) {
+Else if (NumberOfSeats <= PlaneSeats) {
+for var i = i < NumberOfSeats {
+EmptySeat = EmptySeat * 1.05;
+}
+PlaneSeats = PlaneSeats - NumberOfSeats;
+return ("Error,sold out");
+}
+return ("SeatPrice:"+EmptySeat);
+
+var inputHappened = function(currentInput) {
+var NumberOfSeats = parseInt(currentInput);
+if (NumberOfSeats <= 10) {
+display( SeatPrice(NumberOfSeats, EmptySeat))
+} else {
+display("Error,sold out")
+}
